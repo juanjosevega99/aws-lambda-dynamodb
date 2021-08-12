7fcc077cfee1543096144114ba3823001a4b9692
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2021-08-12 11:27:20 -0500

    add create task

---
+++ src/createTask.js
@@ -0,0 +1,30 @@
+const AWS = require('aws-sdk');
+const { v4 } = require('uuid');
+
+const createTask = async event => {
+  const dynamodb = new AWS.DynamoDB.DocumentClient();
+  const { title } = JSON.parse(event.body);
+
+  const newTask = {
+    title: title,
+    id: v4(),
+    createdAt: new Date().toISOString(),
+    done: false
+  };
+
+  await dynamodb
+    .put({
+      TableName: 'TaskTable',
+      Item: newTask
+    })
+    .promise();
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(newTask)
+  };
+};
+
+module.exports = {
+  createTask
+};
